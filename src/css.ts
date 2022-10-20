3bfe34c15603f5e947970a70b93d497229ae827c
Andrew Darts <mandrewdarts@gmail.com>
2022-10-20 17:04:54 -0500

    move css, cleaned up some of the template

--- src/css.ts
+++ src/css.ts
@@ -1,17 +1,11 @@
-export function css(rules: TemplateStringsArray) {
+export function css(rules: string) {
   const sheet = new CSSStyleSheet();
 
-  const rulesArray = rules.reduce((acc: Array<string>, next: string) => {
-    acc.push(
-      ...next
-        .split('}')
-        .map((r) => r.trim())
-        .filter(Boolean)
-        .map((r) => r.concat('}'))
-    );
-
-    return acc;
-  }, []);
+  const rulesArray = rules
+    .split('}')
+    .map((r) => r.trim())
+    .filter(Boolean)
+    .map((r) => r.concat('}'));
 
   for (const rule of rulesArray) {
     sheet.insertRule(rule);
--- src/pokemon-card.ts
+++ src/pokemon-card.ts
@@ -1,112 +1,9 @@
 import { css } from './css';
 import { fetchPokemon } from './pokemon-service';
-
+import sheet from './index.css' assert { type: 'css' };
 const html = String.raw;
 
-const styles = css`
-  :host {
-    --light: var(--pokemon-card-light, white);
-    --dark: var(--pokemon-card-dark, #222);
-    --light-grey: var(--pokemon-card-light-grey, #ddd);
-
-    display: inline-block;
-    width: 250px;
-    height: calc(392px + 50px);
-    border-radius: 0.5rem;
-    overflow: hidden;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-      Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
-      'Segoe UI Symbol';
-    background-color: var(--dark);
-    color: var(--light);
-    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-  }
-
-  header {
-    display: flex;
-    justify-content: space-between;
-    font-weight: bold;
-    padding: 1rem;
-  }
-
-  .hp-name {
-    font-size: 0.5rem;
-    text-transform: uppercase;
-  }
-
-  .hp-value {
-    font-size: 1rem;
-  }
-
-  h1 {
-    margin: 0;
-    padding: 0;
-    text-transform: capitalize;
-    font-size: 1rem;
-  }
-
-  .card-content {
-    background-color: var(--light);
-    color: var(--dark);
-    padding: 1rem;
-  }
-
-  .img-container {
-    display: flex;
-    justify-content: center;
-    height: 200px;
-  }
-
-  img {
-    width: 200px;
-    height: 200px;
-    height: auto;
-  }
-
-  .stats-header {
-    font-size: 0.65rem;
-    font-weight: bold;
-    text-transform: uppercase;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-    margin: 0;
-  }
-
-  li.stat-item {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    font-size: 0.75rem;
-    text-transform: uppercase;
-    height: 2rem;
-  }
-
-  progress {
-    border: none;
-    height: 0.5rem;
-  }
-
-  progress::-webkit-progress-bar {
-    background-color: var(--light-grey);
-    border-radius: 9999px;
-  }
-  progress::-webkit-progress-value {
-    border-radius: 9999px;
-    background-color: var(--dark);
-  }
-
-  .stat-name {
-    width: 100px;
-  }
-
-  .stat-value {
-    margin-left: 1rem;
-    font-weight: bold;
-  }
-`;
+const styles = css(sheet);
 
 export class PokemonCardHTMLElement extends HTMLElement {
   static observedAttributes = ['character'];
@@ -118,7 +15,7 @@ export class PokemonCardHTMLElement extends HTMLElement {
   data: any = null;
   loading = true;
 
-  get character() {
+  getCharacter() {
     return this.getAttribute('character')!;
   }
 
@@ -130,13 +27,13 @@ export class PokemonCardHTMLElement extends HTMLElement {
   }
 
   fetchAndRender() {
-    if (PokemonCardHTMLElement.cache[this.character]) {
-      this.data = PokemonCardHTMLElement.cache[this.character];
+    if (PokemonCardHTMLElement.cache[this.getCharacter()]) {
+      this.data = PokemonCardHTMLElement.cache[this.getCharacter()];
       this.loading = false;
       this.render();
     } else {
-      fetchPokemon(this.character).then((res) => {
-        PokemonCardHTMLElement.cache[this.character] = res;
+      fetchPokemon(this.getCharacter()).then((res) => {
+        PokemonCardHTMLElement.cache[this.getCharacter()] = res;
         this.data = res;
         this.loading = false;
         this.render();
@@ -153,6 +50,9 @@ export class PokemonCardHTMLElement extends HTMLElement {
       this.shadowRoot!.innerHTML = 'Loading...';
     } else {
       const hp = this.data.stats.find((stat: any) => stat.stat.name === 'hp');
+      const stats = this.data.stats.filter(
+        (stat: any) => stat.stat.name !== 'hp'
+      );
       this.shadowRoot!.innerHTML = html`
         <header>
           <h1>${this.data?.name}</h1>
@@ -168,25 +68,29 @@ export class PokemonCardHTMLElement extends HTMLElement {
             />
           </div>
           <ul class="stat-list">
-            ${this.data.stats
-              .filter((stat: any) => stat.stat.name !== 'hp')
-              .map((stat: any) => {
-                return html`
-                  <li class="stat-item">
-                    <span class="stat-name"
-                      >${formatStatNames(stat.stat.name)}</span
-                    >
-                    <progress max="100" value="${stat.base_stat}"></progress>
-                    <span class="stat-value">${stat.base_stat}</span>
-                  </li>
-                `;
-              })
+            ${stats
+              .map((stat: any) =>
+                statListItem({
+                  name: formatStatNames(stat.stat.name),
+                  stat: stat.base_stat,
+                })
+              )
               .join('')}
           </ul>
         </div>
       `;
     }
   }
+}
+
+function statListItem({ name, stat }: any) {
+  return html`
+    <li class="stat-item">
+      <span class="stat-name">${name}</span>
+      <progress max="100" value="${stat}"></progress>
+      <span class="stat-value">${stat}</span>
+    </li>
+  `;
 }
 
 function formatStatNames(name: string) {
